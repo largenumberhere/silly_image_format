ba7dbf07c3c86653b74883e642b814053182d77f
largenumberhere <largenumbergoeshere@gmail.com>
2025-03-11 06:02:21 +1100

    Bug fixes and code cleanup - fix scaling not working correctly until first resize - removed some old printf debugging - added more comments - freeing all resources - window initalized immediately

--- src/view.cpp
+++ src/view.cpp
@@ -52,8 +52,16 @@ int main(int argc, char* argv[]) {
     const int MIN_WIDTH = 800;
     const int MIN_HEIGHT = 600;
 
-    std::vector<const char*> args = collectArgs(argc, argv);
+    // create a raylib context
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT);
+    InitWindow(MIN_WIDTH, MIN_HEIGHT, "silly image format");    
+    BeginDrawing();
+    {
+        DrawText("Loading image...", 10, 10, 20, RAYWHITE);
+    }
+    EndDrawing();
 
+    std::vector<const char*> args = collectArgs(argc, argv);
     if (args.size() != 2) {
         exitWithUsage();
     }
@@ -66,11 +74,10 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
 
-    // fetch all integers
+    // fetch all integers from file
     auto regex = new std::regex ("\\d+", std::regex_constants::ECMAScript | std::regex_constants::icase);
     auto iter_begin = new std::sregex_iterator(file_contents->begin(), file_contents->end(), *regex);
     auto iter_end = std::sregex_iterator();
-    std::cout << "Found " << std::distance(*iter_begin, iter_end) << " words:\n";
     
     // recover the image bytes from the red blue and green values
     std::vector<unsigned char> data;
@@ -95,7 +102,7 @@ int main(int argc, char* argv[]) {
     delete iter_begin;
     delete regex;
     
-    // swap the blue and green channels
+    // swap the blue and green channels due to the format having a different order
     // red, blue green -> red green blue
     for (int index = 0; index < data.size(); index+=4) {
         Color* color = (Color*)&data.at(index);
@@ -103,8 +110,6 @@ int main(int argc, char* argv[]) {
         color->g = tmp.b;
         color->b = tmp.g;
     }
-    
-
     
     // count the rows
     auto regex_rows = new std::regex("begin Row", std::regex_constants::ECMAScript | std::regex_constants::icase);
@@ -116,6 +121,9 @@ int main(int argc, char* argv[]) {
         rows ++;
     }
 
+    // file conversion is complete
+    Color* image_colours = (Color*)data.data();
+
     // cleanup memory
     delete rows_begin;
     delete regex_rows;
@@ -125,27 +133,37 @@ int main(int argc, char* argv[]) {
     int width = (data.size()/4) / rows;
     int height = rows;
 
-    SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT);
-    InitWindow(std::max(width, MIN_WIDTH), std::max(height, MIN_HEIGHT), "silly image format");
-    
+    // update window size
+    if ((width > MIN_WIDTH) || (height > MIN_HEIGHT)) {
+        SetWindowSize(std::max(width, MIN_WIDTH), std::max(height, MIN_HEIGHT));
+    }
+
     // create a texture
     Texture2D texture = genTexture2d(width, height);
-    UpdateTexture(texture, data.data());
+    UpdateTexture(texture, image_colours);
+
+    // create a camera
+
 
     Camera2D camera = {0};
     camera.offset = Vector2{0, 0};
     camera.rotation = 0;
     camera.target = Vector2{0,0};
-    camera.zoom = 1;
 
-    std::string zoom_string;
+    camera.zoom = WINDOW_RELATIVE_SIZE * CAMERA_ZOOM_RATIO;
+
+    Color faint_gray = GRAY;
+    faint_gray.a = 50;
+
+    std::string zoom_format_buffer;
     double prev_key_time = GetTime();
+    bool first_run = true;
     while (!WindowShouldClose())
     {
         
 
         // update camera if window resized
-        if (IsWindowResized()) { 
+        if (IsWindowResized() || first_run) { 
             int new_h = GetScreenHeight();
             int new_w = GetScreenWidth();
             
@@ -169,12 +187,10 @@ int main(int argc, char* argv[]) {
         if (prev_key_time + 0.1 < time) {
             prev_key_time = time;
             if (IsKeyDown('=')) {
-                std::cout << "+\n";
                 CAMERA_ZOOM_RATIO = CAMERA_ZOOM_RATIO * 1.1;
                 camera.zoom = WINDOW_RELATIVE_SIZE * CAMERA_ZOOM_RATIO;
             } else if (IsKeyDown('-')) {
                 CAMERA_ZOOM_RATIO = CAMERA_ZOOM_RATIO * 0.9;
-                std::cout << "-\n";
                 camera.zoom = WINDOW_RELATIVE_SIZE * CAMERA_ZOOM_RATIO;
             }
         }
@@ -189,19 +205,22 @@ int main(int argc, char* argv[]) {
             }
             EndMode2D();
 
-            zoom_string.clear();
-            zoom_string.append(std::format("Zoom: {0}. ", CAMERA_ZOOM_RATIO));
+            zoom_format_buffer.clear();
+            zoom_format_buffer.append(std::format("Zoom: {0}. ", CAMERA_ZOOM_RATIO));
             const char* comment = "(Use +/- to zoom)";
-            int width =  std::max(MeasureText(zoom_string.c_str(), 10), MeasureText(comment, 10));
-            Color faint_gray = GRAY;
-            faint_gray.a = 50;
+            int width =  std::max(MeasureText(zoom_format_buffer.c_str(), 10), MeasureText(comment, 10));
+
             DrawRectangle(0, 0, width+10, 30, faint_gray);
-            DrawText(zoom_string.c_str(), 10, 10, 10, WHITE);
+            DrawText(zoom_format_buffer.c_str(), 10, 10, 10, WHITE);
             DrawText("(Use +/- to zoom)", 10, 20, 10, WHITE);
         }
         EndDrawing();
+
+        first_run = false;
     }
 
+    UnloadTexture(texture);
+    data.~vector();
     CloseWindow();
     
 
